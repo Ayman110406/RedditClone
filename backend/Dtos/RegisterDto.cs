b05e847b08b263b85be5c228066771c19f829379
Ayman110406 <162690069+Ayman110406@users.noreply.github.com>
2026-05-13 21:56:21 +0200

    frontend met backend gekoppeld

--- backend/Dtos/RegisterDto.cs
+++ backend/Dtos/RegisterDto.cs
@@ -1,10 +1,18 @@
-﻿namespace RedditClone.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedditClone.Dtos
 {
     public class RegisterDto
     {
-
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
         public string Password { get; set; }
 
     }
