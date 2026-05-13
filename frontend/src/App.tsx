acb8de0eee1192ba112488c36c403aea8f4c60c0
Ayman110406 <162690069+Ayman110406@users.noreply.github.com>
2026-05-13 21:58:12 +0200

    frontend met backend gekoppeld2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import Homepage from './pages/Homepage.tsx'
 import Loginpage from './pages/Loginpage.tsx'
 import Registerpage from './pages/Registerpage.tsx'
+import Startpage from './pages/Startpage.tsx'
+import ProtectedRoute from './components/ProtectedRoute.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
@@ -9,9 +11,14 @@ function App() {
       <>
           <BrowserRouter>
               <Routes>
-                  <Route path='/' element={<Homepage />} />
+                  <Route path='/' element={<Startpage />} />
                   <Route path='/login' element={<Loginpage />} />
                   <Route path='/register' element={<Registerpage />} />
+                  <Route path='/home' element={
+                      <ProtectedRoute>
+                          <Homepage />
+                      </ProtectedRoute>
+                  } />
               </Routes>
           </BrowserRouter> 
     </>
--- frontend/src/pages/Registerpage.tsx
+++ frontend/src/pages/Registerpage.tsx
@@ -1,11 +1,47 @@
 import Navbar from "../components/Navbar"
 import GegevensVeld from "../components/GegevensVeld"
+import { register } from '../services/AuthService'
+import { useState } from 'react'
+import { useNavigate } from "react-router-dom"
 
 function Registerpage() {
+    const [error, setError] = useState<string | null>(null)
+    const navigate = useNavigate()
+    const handleRegister = async (username: string, email: string, password: string, confirmpassword: string) => {
+
+        if (!/^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(email)) {
+            setError('Ongeldig emailadres')
+            return;
+        }
+
+        if (password.length < 6) {
+            setError("De wachtwoord moet minimaal 6 tekens bevatten");
+            return;
+        }
+
+        if (password !== confirmpassword) {
+            setError("De wachtwoorden komen niet overeen");
+            return;
+        }
+
+        const response = await register(username, email, password)
+        if (response.ok) {
+            navigate('/login');
+        } else {
+            response.json().then(data => {
+                setError(data.message || 'Registratie mislukt')
+            })
+        }
+
+
+
+    }
+
+
     return (
         <>
             <Navbar />
-            <GegevensVeld mode="register" />
+            <GegevensVeld mode="register" onSubmit={handleRegister} error={error} />
         </>
     )
 } export default Registerpage
